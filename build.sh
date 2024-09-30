37c6e6137801c47b6b6c7e8e0246eace1c573348
Alan MacMasters <theravindudeshan@gmail.com>
2024-09-30 04:58:22 +0000

    Introduced a new build script

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+export RDIR=$(pwd)
+export ARCH=arm64
+
+#symlinking python2
+if [ ! -f "$HOME/python" ]; then
+    ln -s /usr/bin/python2.7 "$HOME/python"
+fi 
+
+#proton-12
+if [ ! -d "${RDIR}/toolchains" ]; then
+    mkdir -p "${RDIR}/toolchains"
+    git clone --depth=1 https://github.com/kdrag0n/proton-clang.git --single-branch toolchains/clang
+fi
+
+export PATH=$PWD/toolchains/clang/bin:$PATH
+
+#output dir
+if [ ! -d "${RDIR}/out" ]; then
+    mkdir -p "${RDIR}/out"
+fi
+
+export ARGS="
+-j$(nproc) \
+O=$(pwd)/out \
+ARCH=arm64 \
+CC=clang \
+CROSS_COMPILE=aarch64-linux-gnu- \
+CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+"
+
+build_kernel(){
+    make ${ARGS} acrux_defconfig
+    make ${ARGS} menuconfig
+    make ${ARGS}
+}
+
+build_kernel
